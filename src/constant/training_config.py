5509fe29c78f5b513ca3f8f252409cdc0ecd18fe
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-05-09 23:15:23 +0530

    feat: add data validation directory constants

--- src/constant/training_config.py
+++ src/constant/training_config.py
@@ -9,14 +9,21 @@ ARTIFACT_DIR: str = "PG_Artifacts"
 TRAIN_FILE_NAME: str = "train.csv"
 TEST_FILE_NAME: str = "test.csv"
 RAW_FILE_NAME: str = "raw.csv"
+DRIFT_REPORT_NAME: str = "report.yaml"
+SCHEMA_FILE_PATH: str = os.path.join("data config", "schema.yaml")
 
 # MongoDB Config
 MONGO_DB_DATABASE_NAME: str = os.getenv("DATABASE_NAME")
 MONGO_DB_COLLECTION_NAME: str = os.getenv("COLLECTION_NAME")
 
 # Data Ingestion Config
-DATA_INGESTION_DIR_NAME: str = "Data_Ingestion"
+DATA_INGESTION_DIR_NAME: str = "1. Data Ingestion"
 DATA_INGESTION_INGESTED_DIR: str = "Ingested"
 DATA_INGESTION_RAW_DIR: str = "Raw"
 DATA_INGESTION_TRAIN_TEST_SPLIT_RATIO: float = 0.2
 
+# Data Validation Config
+DATA_VALIDATION_DIR_NAME: str = "2. Data Validation"
+DATA_VALIDATION_VALID_DIR_NAME: str = "Valid Dataset"
+DATA_VALIDATION_INVALID_DIR_NAME: str = "Invalid Dataset"
+DATA_VALIDATION_DRIFT_REPORT_DIR_NAME: str = "Drift Report"
