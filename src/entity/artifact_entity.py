2625d408ded53aa23c910dd530f9d423730b31c9
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-05-12 13:34:53 +0530

    feat: add ModelTrainerConfig dataclass

--- src/entity/artifact_entity.py
+++ src/entity/artifact_entity.py
@@ -50,8 +50,43 @@ class DataTransformationArtifact:
         - transformed_train_file_path (str): Path to transformed train data.
         - transformed_test_file_path (str): Path to transformed test data.
         - transformed_object_file_path (str): Path to preprocessing object.
+
     """
 
     transformed_train_file_path: str
     transformed_test_file_path: str
     transformed_object_file_path: str
+
+
+@dataclass
+class ClassificationModelArtifact:
+    """
+    Stores evaluation metrics for a classification model.
+
+    Attributes:
+        - f1_score (float): Harmonic mean of precision and recall, providing a balance between the two metrics.
+        - precision_score (float): Ratio of correctly predicted positive observations to the total predicted positives.
+        - recall_score (float):  Ratio of correctly predicted positive observations to all actual positive observations.
+
+    """
+
+    f1_score: float
+    precision_score: float
+    recall_score: float
+
+
+@dataclass
+class ModelTrainerArtifact:
+    """
+    Stores artifacts generated during the model training phase.
+
+    Attributes:
+        - trained_model_file_path (str): File path where the trained model is saved.
+        - train_metric_artifact (ClassificationModelArtifact): Evaluation metrics computed on the training dataset.
+        - test_metric_artifact (ClassificationModelArtifact): Evaluation metrics computed on the testing dataset.
+
+    """
+
+    trained_model_file_path: str
+    train_metric_artifact: ClassificationModelArtifact
+    test_metric_artifact: ClassificationModelArtifact
