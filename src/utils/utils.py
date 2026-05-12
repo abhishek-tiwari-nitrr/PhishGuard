95e78c542f1ff86a5b42160fc1ec4091e301b828
beery4010 <abhishek.tiwari.nitrr@gmail.com>
2026-05-12 13:52:35 +0530

    feat: add utility functions for loading NumPy array, loading object, model evaluation and classification score

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -4,6 +4,9 @@ import numpy as np
 import pickle
 from src.exception import ApplicationException
 from src.logger import logger
+from sklearn.metrics import accuracy_score, f1_score, precision_score, recall_score
+from sklearn.model_selection import GridSearchCV
+from src.entity.artifact_entity import ClassificationModelArtifact
 
 
 def read_yaml_file(file_path: str) -> dict:
@@ -75,7 +78,7 @@ def save_numpy_array_data(file_path: str, array: np.ndarray) -> None:
 
     Raises:
         ApplicationException: If saving the NumPy array fails.
-    
+
     """
     try:
         with open(file_path, "wb") as file:
@@ -94,11 +97,141 @@ def save_object(file_path: str, obj: object) -> None:
 
     Raises:
         ApplicationException: If object serialization or saving fails.
-        
+
     """
     try:
         with open(file_path, "wb") as file:
             logger.info(f"Saving object to {file}")
             pickle.dump(obj, file)
+    except Exception as e:
+        raise ApplicationException(e, sys)
+
+
+def load_numpy_array_data(file_path: str) -> np.ndarray:
+    """
+    Load a NumPy array from a binary file.
+
+    Args:
+        file_path (str): Path to the NumPy binary file to be loaded.
+
+    Raises:
+        ApplicationException: If opening/loading the NumPy array fails.
+
+    Returns:
+        np.ndarray: Loaded NumPy array.
+    """
+    try:
+        with open(file_path, "rb") as file:
+            return np.load(file)
+    except Exception as e:
+        raise ApplicationException(e, sys)
+
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    """
+    Train and evaluate multiple classification models using GridSearchCV.
+
+    Args:
+        X_train (array): Training feature dataset.
+        y_train (array): Training target labels.
+        X_test (array): Testing feature dataset.
+        y_test (array): Testing target labels.
+        models (dict): Dictionary containing model names as keys and model objects as values.
+        param (dict): Dictionary containing hyperparameter grids for each model.
+
+    Raises:
+        ApplicationException: Raised if model training or evaluation fails.
+
+    Returns:
+        dict: Dictionary containing evaluation metrics for each model.
+
+    """
+    try:
+        report = {}
+        for model_name, model in models.items():
+            para = param[model_name]
+
+            gs = GridSearchCV(model, para, cv=3, scoring="f1_weighted")
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
+
+            model.fit(X_train, y_train)
+
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            train_f1 = f1_score(y_train, y_train_pred, average="weighted")
+            test_f1 = f1_score(y_test, y_test_pred, average="weighted")
+
+            precision = precision_score(
+                y_test, y_test_pred, average="weighted", zero_division=0
+            )
+
+            recall = recall_score(
+                y_test, y_test_pred, average="weighted", zero_division=0
+            )
+
+            accuracy = accuracy_score(y_test, y_test_pred)
+
+            report[model_name] = {
+                "train_f1_score": train_f1,
+                "test_f1_score": test_f1,
+                "precision_score": precision,
+                "recall_score": recall,
+                "accuracy_score": accuracy,
+                "best_params": gs.best_params_,
+            }
+
+        return report
+    except Exception as e:
+        raise ApplicationException(e, sys)
+
+
+def get_classification_score(y_true, y_pred) -> ClassificationModelArtifact:
+    """
+    Calculate classification evaluation metrics.
+
+    Args:
+        y_true (array): Actual target labels.
+        y_pred (array): Predicted target labels.
+
+    Raises:
+        ApplicationException: Raised if evaluation fails.
+
+    Returns:
+        ClassificationModelArtifact: Object containing F1-score, precision, and recall.
+
+    """
+    try:
+        return ClassificationModelArtifact(
+            f1_score=f1_score(y_true, y_pred, average="weighted"),
+            precision_score=precision_score(
+                y_true, y_pred, average="weighted", zero_division=0
+            ),
+            recall_score=recall_score(
+                y_true, y_pred, average="weighted", zero_division=0
+            ),
+        )
+    except Exception as e:
+        raise ApplicationException(e, sys)
+
+
+def load_object(file_path: str) -> object:
+    """
+    Load and deserialize a Python object from a pickle file.
+
+    Args:
+        file_path (str): Path to the pickle file containing the serialized object.
+
+    Raises:
+        ApplicationException: Raised if object cannot be loaded.
+
+    Returns:
+        object: The deserialized Python object loaded from the file.
+    """
+    try:
+        with open(file_path, "rb") as file:
+            return pickle.load(file)
     except Exception as e:
         raise ApplicationException(e, sys)
